8fe1da41219ebd45d2fb05798fa4e72b5bec09ec
Vergil <88496530+CodeLynch@users.noreply.github.com>
2023-05-12 10:01:57 +0800

    Changed to backend

---
+++ src/main/java/com/capstone/datamate/DatamateApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.datamate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatamateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatamateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/datamate/DatamateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.datamate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatamateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
