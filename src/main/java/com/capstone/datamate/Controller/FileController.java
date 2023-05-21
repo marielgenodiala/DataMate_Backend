d83f41b4df0762d17d48dc2d4915a8ae1f60183b
Vergil <88496530+CodeLynch@users.noreply.github.com>
2023-05-21 18:43:21 +0800

    Controller Updated

--- src/main/java/com/capstone/datamate/Controller/FileController.java
+++ src/main/java/com/capstone/datamate/Controller/FileController.java
@@ -7,12 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+// import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -21,8 +22,9 @@ import com.capstone.datamate.Message.ResponseFile;
 import com.capstone.datamate.Message.ResponseMessage;
 import com.capstone.datamate.Service.FileService;
 
-@Controller
-@CrossOrigin
+
+@CrossOrigin("http://localhost:3000")
+@RestController
 public class FileController {
     @Autowired
   FileService fileService;
