f3b8ab2f0dec0278f87445d04277846b6ad8a332
citmarielgenodiala <genodialamariel1701@gmail.com>
2023-11-05 02:29:10 +0800

    update DatamateApplication

--- src/main/java/com/capstone/datamate/DatamateApplication.java
+++ src/main/java/com/capstone/datamate/DatamateApplication.java
@@ -2,12 +2,20 @@ package com.capstone.datamate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+//@SpringBootApplication
+//public class DatamateApplication{
+//	
+//	public static void main(String[] args){
+//		SpringApplication.run(DatamateApplication.class, args);
+//	}
+//
+//}
 @SpringBootApplication
-public class DatamateApplication{
-	
-	public static void main(String[] args){
-		SpringApplication.run(DatamateApplication.class, args);
-	}
-
+@ComponentScan(basePackages = "com.capstone.datamate")
+public class DatamateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DatamateApplication.class, args);
+    }
 }
