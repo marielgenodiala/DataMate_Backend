5e4536a8c5e0084c3cc7d87b1ff648620491fd83
citmarielgenodiala <genodialamariel1701@gmail.com>
2023-11-05 02:28:03 +0800

    adding forgot password functions

--- src/main/java/com/capstone/datamate/Service/UserService.java
+++ src/main/java/com/capstone/datamate/Service/UserService.java
@@ -1,9 +1,11 @@
 package com.capstone.datamate.Service;
 
 import java.io.IOException;
+import java.time.Instant;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,12 +13,23 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.capstone.datamate.Entity.UserEntity;
 import com.capstone.datamate.Repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.Duration;
 
 @Service
 public class UserService {
 	
 	@Autowired
 	UserRepository urepo;
+	private final EmailService emailService;
+
+	@Autowired
+	public UserService(EmailService emailService) {
+	    this.emailService = emailService;
+	}
+
+
 	
 	// create user
 //	public UserEntity insertUser(UserEntity user, MultipartFile userImage) throws IOException{
@@ -87,5 +100,63 @@ public class UserService {
 		
 		return msg;
 	}
+	
+	
+	
+	@Transactional
+	public UserEntity findUserByEmail(String email) {
+	    UserEntity user = urepo.findByEmail(email);
+	    if (user == null) {
+	        System.out.println("User not found for email: " + email);
+	    }
+	    return user;
+	}
+
+
+	@Transactional
+	public void generateAndSendVerificationCode(UserEntity user) {
+	    String verificationCode = generateRandomCode();
+	    user.setVerificationCode(verificationCode);
+	    Instant expirationTime = Instant.now().plus(Duration.ofMinutes(5)); 
+	    user.setVerificationCodeExpiration(expirationTime);
+	    urepo.save(user);
+	    emailService.sendVerificationCode(user.getEmail(), verificationCode);
+	}
+
+
+	@Transactional
+	public boolean isVerificationCodeValid(UserEntity user, String enteredCode) {
+	    String storedCode = user.getVerificationCode();
+	    Instant expirationTime = user.getVerificationCodeExpiration();
+
+	    if (storedCode != null && storedCode.equals(enteredCode) && expirationTime != null && expirationTime.isAfter(Instant.now())) {
+	        return true; 
+	    }
+
+	    return false;
+	}
+
+
+
+   @Transactional
+   public void resetPassword(UserEntity user, String newPassword) {
+       String currentPassword = user.getPassword();
+
+       if (!currentPassword.equals(newPassword)) {
+           user.setPassword(newPassword);
+           user.setVerificationCode(null);
+           urepo.save(user);
+       } else {
+           throw new IllegalArgumentException("New password must be different from current password.");
+       }
+   }
+
+
+    private String generateRandomCode() {
+        Random random = new Random();
+        int code = 100000 + random.nextInt(900000);
+        return String.valueOf(code);
+    }
+
 
 }
