7dd2f4e8f8671bb2cc079410424349d31711af2b
Cara Carmel Encabo <caracarmel.encabo@cit.edu>
2023-11-04 10:46:18 +0800

    Update FileServiceImpl to include logs

--- src/main/java/com/capstone/datamate/Service/FileServiceImpl.java
+++ src/main/java/com/capstone/datamate/Service/FileServiceImpl.java
@@ -13,8 +13,10 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.capstone.datamate.Entity.FileActivityLogEntity;
 import com.capstone.datamate.Entity.FileEntity;
 import com.capstone.datamate.Entity.UserEntity;
+import com.capstone.datamate.Repository.FileActivityLogRepository;
 import com.capstone.datamate.Repository.FileRepository;
 import com.capstone.datamate.Repository.UserRepository;
 
@@ -25,6 +27,9 @@ public class FileServiceImpl implements FileService {
 
   @Autowired
   FileRepository fileRepo;
+  
+  @Autowired
+  FileActivityLogRepository logRepository;
   
 
   @Autowired
@@ -59,7 +64,13 @@ public class FileServiceImpl implements FileService {
       if (user != null) {
           FileEntity fileEntity = new FileEntity(fileName, file.getSize(), file.getBytes());
           fileEntity.setUser(user);
-          return fileRepo.save(fileEntity);
+          FileEntity savedFile = fileRepo.save(fileEntity);
+          
+          String activity = fileEntity.getFileName() + " uploaded";
+          FileActivityLogEntity log = new FileActivityLogEntity(savedFile, user, activity);
+          logRepository.save(log);
+          
+          return savedFile;
       } else {
           throw new EntityNotFoundException("User with ID " + userId + " not found");
       }
@@ -108,53 +119,130 @@ public class FileServiceImpl implements FileService {
     return fileRepo.findByIsdeleted(true);
   }
 
-  //permanently remove file
+//  //permanently remove file
+//	public void permanentlyDeleteFile(int id) {
+//	   Optional<FileEntity> optionalFileEntity = fileRepo.findById(id);
+//	
+//	   if (optionalFileEntity.isPresent()) {
+//	       FileEntity fileEntity = optionalFileEntity.get();
+//	       fileRepo.delete(fileEntity);
+//	   } else {
+//	       throw new EntityNotFoundException("File with id " + id + " not found");
+//	   }
+//	}
+  
 	public void permanentlyDeleteFile(int id) {
-	   Optional<FileEntity> optionalFileEntity = fileRepo.findById(id);
+	  Optional<FileEntity> optionalFileEntity = fileRepo.findById(id);
 	
+	  if (optionalFileEntity.isPresent()) {
+	      FileEntity fileEntity = optionalFileEntity.get();
+	
+	      if (fileEntity.getIsdeleted()) {
+	          UserEntity user = fileEntity.getUser();
+	
+	          String activity = fileEntity.getFileName() + " deleted";
+	          FileActivityLogEntity log = new FileActivityLogEntity(fileEntity, user, activity);
+	          logRepository.save(log);
+	
+	          fileRepo.delete(fileEntity);
+	      } else {
+	          throw new EntityNotFoundException("File with id " + id + " is not marked as deleted and cannot be permanently deleted.");
+	      }
+	  } else {
+	      throw new EntityNotFoundException("File with id " + id + " not found");
+	  }
+	}
+
+
+	// restore file by id
+//	public FileEntity restoreFile(int id) {
+//	   Optional<FileEntity> optionalFileEntity = fileRepo.findById(id);
+//	
+//	   if (optionalFileEntity.isPresent()) {
+//	       FileEntity fileEntity = optionalFileEntity.get();
+//	
+//	       fileEntity.setIsdeleted(false);
+//	
+//	       return fileRepo.save(fileEntity);
+//	   } else {
+//	       throw new EntityNotFoundException("File with id " + id + " not found");
+//	   }
+//	}
+	
+	public FileEntity restoreFile(int id) {
+	   Optional<FileEntity> optionalFileEntity = fileRepo.findById(id);
+
 	   if (optionalFileEntity.isPresent()) {
 	       FileEntity fileEntity = optionalFileEntity.get();
-	       fileRepo.delete(fileEntity);
+
+	       if (fileEntity.getIsdeleted()) {
+	           fileEntity.setIsdeleted(false);
+
+	           UserEntity user = fileEntity.getUser();
+	           
+	           String activity = "Restored " + fileEntity.getFileName();
+	           
+	           FileActivityLogEntity log = new FileActivityLogEntity(fileEntity, user, activity);
+	           logRepository.save(log);
+
+	           return fileRepo.save(fileEntity);
+	       } else {
+	           throw new EntityNotFoundException("File with id " + id + " was not deleted and cannot be restored.");
+	       }
 	   } else {
 	       throw new EntityNotFoundException("File with id " + id + " not found");
 	   }
 	}
 
-	// restore file by id
-	public FileEntity restoreFile(int id) {
-	   Optional<FileEntity> optionalFileEntity = fileRepo.findById(id);
-	
-	   if (optionalFileEntity.isPresent()) {
-	       FileEntity fileEntity = optionalFileEntity.get();
-	
-	       fileEntity.setIsdeleted(false);
-	
-	       return fileRepo.save(fileEntity);
-	   } else {
-	       throw new EntityNotFoundException("File with id " + id + " not found");
-	   }
-	}
+
 	
 	// get deleted files by user id
 	public List<FileEntity> getDeletedFilesById(int userId) {
 	    return fileRepo.findDeletedFilesByUserId(userId);
 	}
 
-  @Override
-  public void deleteFile(int id) {
-      Optional<FileEntity> optionalFileEntity = fileRepo.findById(id);
+//  @Override
+//  public void deleteFile(int id) {
+//      Optional<FileEntity> optionalFileEntity = fileRepo.findById(id);
+//
+//      if (optionalFileEntity.isPresent()) {
+//          FileEntity fileEntity = optionalFileEntity.get();
+//
+//          // Set the isdeleted flag to true
+//          fileEntity.setIsdeleted(true);
+//
+//          // Update the entity in the database
+//          fileRepo.save(fileEntity);
+//      } else {
+//          throw new EntityNotFoundException("File with id " + id + " not found");
+//      }
+//  }
+	
+	// temporarily delete file by moving it to bin
+	@Override
+	public void deleteFile(int id) {
+	   Optional<FileEntity> optionalFileEntity = fileRepo.findById(id);
 
-      if (optionalFileEntity.isPresent()) {
-          FileEntity fileEntity = optionalFileEntity.get();
+	   if (optionalFileEntity.isPresent()) {
+	       FileEntity fileEntity = optionalFileEntity.get();
 
-          // Set the isdeleted flag to true
-          fileEntity.setIsdeleted(true);
+	       if (!fileEntity.getIsdeleted()) {
+	           fileEntity.setIsdeleted(true);
 
-          // Update the entity in the database
-          fileRepo.save(fileEntity);
-      } else {
-          throw new EntityNotFoundException("File with id " + id + " not found");
-      }
-  }
+	           UserEntity user = fileEntity.getUser();
+	           	
+	           String activity = fileEntity.getFileName() + " was moved to bin";
+	           FileActivityLogEntity log = new FileActivityLogEntity(fileEntity, user, activity);
+	           logRepository.save(log);
+
+	           fileRepo.save(fileEntity);
+	       } else {
+	           throw new EntityNotFoundException("File with id " + id + " is already deleted.");
+	       }
+	   } else {
+	       throw new EntityNotFoundException("File with id " + id + " not found");
+	   }
+	}
+
 
 }
